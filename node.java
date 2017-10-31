2002bb8528d38e010059f21cd183ab965317b33c
brandonshark <32677373+brandonshark@users.noreply.github.com>
2017-10-31 16:50:23 +0000

    final syntax of node class

--- node.java
+++ node.java
@@ -2,52 +2,53 @@ import java.util.ArrayList;
 
 public class node<x> {
 	public x data;
-    public ArrayList<node<x>> catalogue;
-    public x key;
-    public int in;
-    public int out;
-    
-    public node(x ID,x val)
-    {
-        key = ID;
-        data = val; 
-        catalogue = new ArrayList<node<x>>();
-        in = 0;
-        out = catalogue.size();
-    } 
+	public ArrayList<node<x>> catalogue;
+	public x key;
+	public int in;
+	public int out;
 
-   
-    public void link(node<x> root)
-    {
-        if(root != null)
-        {
-            this.catalogue.add(root);
-            root.in++;
-        }
-        
-    }
+	public node(x ID,x val)
+	{
+		key = ID;
+		data = val; 
+		catalogue = new ArrayList<node<x>>();
+		in = 0;
+		out = catalogue.size();
+	} 
 
-    public void delink(node<x> root)
-    {
-    	if(root!=null)
-    	{
-    		this.catalogue.remove(root);
-    		root.in--;
-    	}
-    }
 
-public void removeAt(ArrayList<node<x>> var)
-{
-    if(var==null) return;
-    for(int i = 0; i < var.size(); i++)
-    {
-        node<x> temp = var.get(i);
-        if(temp.catalogue.contains(this))
-        {
-            temp.catalogue.remove(temp.catalogue.indexOf(this));
-        }
-    }
-    this.catalogue = null;
-}
+	public void link(node<x> root)
+	{
+		if(root != null)
+		{
+			this.catalogue.add(root);
+			root.in++;
+		}
+
+	}
+
+	public void delink(node<x> root)
+	{
+		if(root!=null)
+		{
+			this.catalogue.remove(root);
+			root.in--;
+		}
+	}
+
+	public void removeAt(ArrayList<node<x>> var)
+	{
+		if(var==null) return;
+		for(int i = 0; i < var.size(); i++)
+		{
+			node<x> temp = var.get(i);
+			if(temp.catalogue.contains(this))
+			{
+				temp.catalogue.remove(temp.catalogue.indexOf(this));
+			}
+		}
+		this.catalogue = null;
+	}
+
 }
 
