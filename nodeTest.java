ae7e6bd1ca1835fc53ec0659ed0e9014c103c7d6
brandonshark <32677373+brandonshark@users.noreply.github.com>
2017-10-31 18:12:54 +0000

    Delink Test
    Tests not successful, will complete other tests before attempting to fix

--- nodeTest.java
+++ nodeTest.java
@@ -3,18 +3,39 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class nodeTest {
-@Test
-	 @SuppressWarnings({ "unchecked", "rawtypes" })
+	@Test
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void testLink()
-	  {
-	    node x = new node("x", 3);
-	    node y = new node("y", 2);
-	    x.link(y);
-	    assertTrue(x.catalogue.size()==1);
-	    y.link(x);
-	    assertTrue(y.catalogue.size()==1);
-	    y.link(null);
-	    assertTrue(y.catalogue.size()==1);
-	  }
+	{
+		node x = new node("x", 3);
+		node y = new node("y", 2);
+		x.link(y);
+		assertTrue(x.catalogue.size()==1);
+		y.link(x);
+		assertTrue(y.catalogue.size()==1);
+		y.link(null);
+		assertTrue(y.catalogue.size()==1);
+	}
+
+	@Test
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	
+	public void testUnlink()
+	{
+		node x = new node("x", 3);
+		node y = new node("y", 2);
+		x.link(y);
+		y.link(x);
+		y.link(null);
+		x.unlink(y);
+		assertTrue(x.catalogue.size()==0);
+		assertTrue(y.catalogue.size()==0);
+		y.unlink(x);
+		assertTrue(y.catalogue.size()==0);
+		y.unlink(null);
+		assertTrue(y.catalogue.size()==0);
+	}
 
 }
+
+
