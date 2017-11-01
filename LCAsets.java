f097ece20c94537957e64e3a03c6a24488df4ed4
brandonshark <32677373+brandonshark@users.noreply.github.com>
2017-11-01 16:22:05 +0000

    completed LCAsets class
    Added LCA set method, search method, junction method and impasse method

--- LCAsets.java
+++ LCAsets.java
@@ -1,7 +1,55 @@
 import java.util.ArrayList;
 
 public class LCAsets {
-	
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static ArrayList<node> LCAset(ArrayList<node> nodeCata, node x, node y)
+	{  	
+		if(nodeCata == null || x == null || y == null || nodeCata.size() == 0 ||!nodeCata.contains(x) || !nodeCata.contains(y) ) return null;
+		if(!acyclicVerification(nodeCata)) return null;
+		ArrayList<node> rootCata = new ArrayList<node>();
+		ArrayList<node> ListA = new ArrayList<node>();
+		ArrayList<node> ListB = new ArrayList<node>();
+		ArrayList<node> commonList = new ArrayList<node>();
+		for(int i = 0; i < nodeCata.size(); i++)
+		{
+			if(nodeCata.get(i).in== 0)
+			{
+				rootCata.add(nodeCata.get(i));
+			}
+		}
+		ListA.add(x);
+		ListB.add(y);
+		for(int i = 0; i < rootCata.size(); i++)
+		{
+			search(rootCata.get(i), ListA, ListB);
+		}
+		commonList = junction(ListA, ListB);
+		if(commonList.size() == 0) return null;
+		while(commonList.size() != 1)
+		{
+			for(int i = 0; i < commonList.size(); i++)
+			{
+
+				if(impasse(commonList))
+				{
+					return commonList;
+				}
+				else
+				{
+					if(junction(commonList, commonList.get(i).catalogue).size() != 0)
+					{
+						commonList.remove(i);
+					}
+				}
+			}	
+		}
+		return commonList;
+
+	}
+
+
+
 	@SuppressWarnings("rawtypes")
 	public static boolean acyclicVerification(ArrayList<node> index)
 	{
@@ -22,13 +70,13 @@ public class LCAsets {
 		}
 		return true;
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	private static boolean isCycle(ArrayList<node> list, node temp, ArrayList<node> marked, ArrayList<node> heap, boolean isCycle)
 	{
 		marked.add(temp);
 		heap.add(temp);
-		
+
 		for(int x= 0; x < temp.catalogue.size(); x++)
 		{
 			node y = (node)temp.catalogue.get(x);
@@ -45,6 +93,44 @@ public class LCAsets {
 		heap.remove(temp);
 		return isCycle;
 	}
-	
-	
+
+	@SuppressWarnings("rawtypes")
+	public static ArrayList<node> junction(ArrayList<node> cata1, ArrayList<node> cata2) {
+		ArrayList<node> cata = new ArrayList<node>();
+
+		for (node x : cata1) {
+			if(cata2.contains(x)) {
+				cata.add(x);
+			}
+		}
+		return cata;
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static boolean impasse(ArrayList<node> commonList) 
+	{
+		for(int i = 0; i < commonList.size(); i++)
+		{
+			node temp = commonList.get(i);
+			if(junction(commonList, temp.catalogue).size() != 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static void search(node root, ArrayList<node> ListA, ArrayList<node> ListB)
+	{
+		if(root.catalogue == null || root.catalogue.size() == 0) return;
+
+		for(int i = 0; i < root.catalogue.size(); i++)
+		{
+			node temp = (node) root.catalogue.get(i);
+			if(!(ListA.contains(temp) || ListB.contains(temp))) search(temp, ListA, ListB);
+			if(ListA.contains(temp)) ListA.add(root);
+			if(ListB.contains(temp)) ListB.add(root);	
+		}
+	}
 }
