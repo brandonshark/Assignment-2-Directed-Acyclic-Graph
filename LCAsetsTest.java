b3761b4e06c545095e6d05bcbfec8633bc96bdfd
brandonshark <32677373+brandonshark@users.noreply.github.com>
2017-11-08 19:38:13 +0000

    Syntax Updated on LCAsetsTest class

--- LCAsetsTest.java
+++ LCAsetsTest.java
@@ -34,55 +34,52 @@ public class LCAsetsTest{
 		z.link(z);
 		assertEquals("Verifying not Acyclic", false, LCAsets.acyclicVerification(cycleList));
 	}
-	
+
 	@Test
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	  public void testLCAsets()
-	  {
-	    node x = new node("x",6);
-	    node y = new node("y",9);
-	    node z = new node("z",38);
-	    node l = new node("l",385);
-	    
-	    x.link(y);
-	    y.link(z);
-	    x.link(l);
-	    assertEquals("Testing null inputs", null, LCAsets.LCAset(null, null, null));
-	    assertEquals("Testing null inputs", null, LCAsets.LCAset(null, x, null));
-	    assertEquals("Testing null inputs", null, LCAsets.LCAset(null, null, y));
-	    
-	    ArrayList<node> list = new ArrayList<node>();
-	    assertEquals("Testing empty list", null, LCAsets.LCAset(list, x, y));
-	   
-	    list.add(x);
-	    list.add(y);
-	    assertEquals("Testing with node not in list", null, LCAsets.LCAset(list, z, y));
-	    list.add(z);
-	    list.add(l);
-	    ArrayList<node> answers = new ArrayList<node>();
-	    answers.add(x);
-	    assertEquals("Testing LCA with one Common Ancestor.", false, answers.retainAll(LCAsets.LCAset(list, z, l)));
-	    
-	    node j = new node("j",250);
-	    j.link(y);
-	    list.add(j);
-	    assertEquals("Testing LCA with one Common Ancestor.", false, answers.retainAll(LCAsets.LCAset(list, z, l)));
-	    j.link(l);
-	    answers.add(j);
-	    assertEquals("Testing LCA with two Common Ancestors.", false, answers.retainAll(LCAsets.LCAset(list, z, l)));
-	    
-	    node k = new node("k", 235);
-	    k.link(x);
-	    k.link(z);
-	    list.add(k);
-	    assertEquals("Testing LCA with two Common Ancestors.", false, answers.retainAll(LCAsets.LCAset(list, z, l)));
+	public void testLCAsets()
+	{
+		node x = new node("x",6);
+		node y = new node("y",9);
+		node z = new node("z",38);
+		node l = new node("l",385);
 
-	    y.link(x);
-	    z.link(k);
-	    assertEquals("Testing LCA with x cycle.", null, LCAsets.LCAset(list, z ,l));
-	    y.unlink(x);
-	  }
+		x.link(y);
+		y.link(z);
+		x.link(l);
+		assertEquals("Testing null inputs", null, LCAsets.LCAset(null, null, null));
+		assertEquals("Testing null inputs", null, LCAsets.LCAset(null, x, null));
+		assertEquals("Testing null inputs", null, LCAsets.LCAset(null, null, y));
 
+		ArrayList<node> list = new ArrayList<node>();
+		assertEquals("Testing empty list", null, LCAsets.LCAset(list, x, y));
 
+		list.add(x);
+		list.add(y);
+		assertEquals("Testing with node not in list", null, LCAsets.LCAset(list, z, y));
+		list.add(z);
+		list.add(l);
+		ArrayList<node> answers = new ArrayList<node>();
+		answers.add(x);
+		assertEquals("Testing LCA with one Common Ancestor.", false, answers.retainAll(LCAsets.LCAset(list, z, l)));
 
+		node j = new node("j",250);
+		j.link(y);
+		list.add(j);
+		assertEquals("Testing LCA with one Common Ancestor.", false, answers.retainAll(LCAsets.LCAset(list, z, l)));
+		j.link(l);
+		answers.add(j);
+		assertEquals("Testing LCA with two Common Ancestors.", false, answers.retainAll(LCAsets.LCAset(list, z, l)));
+
+		node k = new node("k", 235);
+		k.link(x);
+		k.link(z);
+		list.add(k);
+		assertEquals("Testing LCA with two Common Ancestors.", false, answers.retainAll(LCAsets.LCAset(list, z, l)));
+
+		y.link(x);
+		z.link(k);
+		assertEquals("Testing LCA with x cycle.", null, LCAsets.LCAset(list, z ,l));
+		y.unlink(x);
+	}
 }
